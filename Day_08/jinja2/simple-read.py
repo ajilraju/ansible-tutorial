16f66b36c1c2ee63484546917fe570a8dc87f799
Ajil Raju <ajilraju.m@gmail.com>
2023-01-30 18:59:29 +0530

    Added ansible and Jinja2 examples

---
+++ Day_08/jinja2/simple-read.py
@@ -0,0 +1,14 @@
+from os import environ
+from jinja2 import FileSystemLoader, Template, Environment
+
+bio_data = {
+    "first_name": "Harry",
+    "last_name": "Stark"
+}
+
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+template = env.get_template('fill-in-the-blank')
+
+print(template.render(bio_data))
+
